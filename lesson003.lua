ffc99afe00931fd84b0fdc43e9e9408cd2929716
FoXcodeZ <m.grabowy@int.pl>
2024-12-13 02:31:18 +0100

    Added lesson003 - commnets

---
+++ lesson003.lua
@@ -0,0 +1,10 @@
+-- This is a single-line comment.
+-- print("This will not be printed.")
+print("This will be printed.")
+
+-- This is a multi-line comment.
+--[[
+    print("This will be printed.")
+    print("This will also be printed.")
+]]
+print("We can print this text too.")
